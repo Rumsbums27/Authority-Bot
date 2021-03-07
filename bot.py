1cdc08477a04b98354ce2382d2dfe8527a78bd3d
Rumsbums27 <felix.waidler@web.de>
2021-03-07 17:02:54 +0100

    added error listener

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import asyncio
 import discord
-from discord import Intents
+from discord import Intents, Embed
 from discord.ext import commands
 from cogs import COGS, disabled
 import json
@@ -28,6 +28,16 @@ async def status_task():
         await asyncio.sleep(5)
         await bot.change_presence(activity=discord.Game('help'), status=discord.Status.online)
         await asyncio.sleep(5)
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    embed = Embed(
+        title='Error',
+        description=str(error),
+        color=0xff0000
+    )
+    await ctx.send(embed=embed)
 
 for cogs in COGS:
     if cogs in disabled:
--- cogs/__init__.py
+++ cogs/__init__.py
@@ -56,4 +56,5 @@ COGS = [
 
 disabled = [
     TranslateCog,
+    VoiceCog
 ]
